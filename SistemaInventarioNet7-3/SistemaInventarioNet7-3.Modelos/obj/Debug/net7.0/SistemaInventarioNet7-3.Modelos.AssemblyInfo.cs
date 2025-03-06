177c025a155e4a1a8125b4ba0f343bc64ce7747c
ciso2020 <cisoivan2020@gmail.com>
2025-03-06 15:42:40 -0600

    Carro de Compras - Pagos

--- SistemaInventarioNet7-3/SistemaInventarioNet7-3.Modelos/obj/Debug/net7.0/SistemaInventarioNet7-3.Modelos.AssemblyInfo.cs
+++ SistemaInventarioNet7-3/SistemaInventarioNet7-3.Modelos/obj/Debug/net7.0/SistemaInventarioNet7-3.Modelos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventarioNet7-3.Modelos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f58bb90408750191b193f571dccf379ded36b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950d9ad68398e4de9288cc914e852158efd47814")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventarioNet7-3.Modelos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventarioNet7-3.Modelos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
